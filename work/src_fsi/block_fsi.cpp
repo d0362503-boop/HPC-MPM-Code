2c458697b29d63543d7963e0e2cce77421fd3701
pan <dev@example.com>
2026-08-07 11:37:19 +0800

    format unify

--- work/src_fsi/block_fsi.cpp
+++ work/src_fsi/block_fsi.cpp
@@ -116,6 +116,7 @@ void BlockFSI::SolveFSISystem() {
 }
 
 void BlockFSI::CalFSIResidual(double &rtr_ref, double &rtr_dof, const std::vector<double> &nvel_k) {
+
     double norm = 0.0e0, intf_num = 0.0e0;
     for (int n = 0; n < this->fsi_intf.ibc; n++) {
         int nid = this->fsi_intf.nbc[n];
@@ -124,9 +125,9 @@ void BlockFSI::CalFSIResidual(double &rtr_ref, double &rtr_dof, const std::vecto
         double us2 = nvel_k[nid + nvc];
         double us3 = nvel_k[nid + nwc];
 
-        double uf1 = fluid_.nvel[nid + nuc];
-        double uf2 = fluid_.nvel[nid + nvc];
-        double uf3 = fluid_.nvel[nid + nwc];
+        double uf1 = this->fluid_.nvel[nid + nuc];
+        double uf2 = this->fluid_.nvel[nid + nvc];
+        double uf3 = this->fluid_.nvel[nid + nwc];
 
         double dr1 = us1 - uf1;
         double dr2 = us2 - uf2;
@@ -356,6 +357,7 @@ void BlockFSI::CalFSIForce() {
 }
 
 double FSISolid::ComputeNRLumpedMassMat(int pid, double sfi) const noexcept {
+
     double emd = MaterialPoint::ComputeNRLumpedMassMat(pid, sfi);
     emd += this->nb_para[3] * sfi * this->fsi_.fluid_.rhol * this->vol[pid];
 
@@ -363,6 +365,7 @@ double FSISolid::ComputeNRLumpedMassMat(int pid, double sfi) const noexcept {
 }
 
 std::array<double, 3> FSISolid::ComputeExternalForce(int pid, double sfi) const noexcept {
+
     double fx = bb[0] * facl;
     double fy = bb[1] * facl;
     double fz = bb[2] * facl;
@@ -376,8 +379,8 @@ std::array<double, 3> FSISolid::ComputeExternalForce(int pid, double sfi) const 
 }
 
 void FSISolid::AddInertialForceToRHS(CrsMat &mat, const std::vector<double> &naccel) {
+
     for (int n = 0; n < nodec; n++) {
-        // --- For Generalized-α (if α_m = 1, back to Newmark-β) ---
         mat.b_rhs[n + nuc] -= (this->nmass[n] + this->fsi_.added_mass[n]) * naccel[n + nuc];
         mat.b_rhs[n + nvc] -= (this->nmass[n] + this->fsi_.added_mass[n]) * naccel[n + nvc];
         mat.b_rhs[n + nwc] -= (this->nmass[n] + this->fsi_.added_mass[n]) * naccel[n + nwc];
